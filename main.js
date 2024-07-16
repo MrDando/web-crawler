a06de0544c6d1e4ce794b8a24a513f17a8e589e6
Danijel Stanić <danijel.stanich@gmail.com>
2024-07-16 20:21:32 +0200

    added CLI argument to main function

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-import { normalizeURL } from "./crawl.js"
+import { normalizeURL } from './crawl.js'
+import { argv } from 'node:process';
 
-console.log('Starting web crawler')
+function main() {
+    console.log('--------------------------------------------------------------')
+    console.log('Starting web crawler')
 
-normalizeURL('https://www.youtube.com/feed/history')
+    if (argv.length < 3) {
+        console.error("Please provide URL")
+        return
+    } else if (argv.length > 3) {
+        console.error("Program expects only one CLI argmunet")
+        return
+    }
+
+    const url = argv[2]
+    console.log(`Starting web-crawler with URL: ${url}`)
+
+  }
+  
+  main()
+  console.log('--------------------------------------------------------------')
+
+
+
+// print process.argv
+// argv.forEach((val, index) => {
+//     console.log(`${index}: ${val}`);
+//   });
